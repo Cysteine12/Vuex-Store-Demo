98a6a08672af4c2a5f1ddcd179c66321156e598a
Cysteine12 <gbengapeace2001@gmail.com>
2022-01-28 23:41:16 +0100

    1 file modified, 2 added, 1 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+    <Box></Box>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Box from './components/Box.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Box
 }
 </script>
 
